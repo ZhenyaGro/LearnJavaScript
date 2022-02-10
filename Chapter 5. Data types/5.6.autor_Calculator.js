e5a4c83c56b6d103e3917e0ac4b05c89e7d94213
ZhenyaGro <bon4ios@gmail.com>
2022-02-10 11:59:11 +0300

    Improved task text

--- Chapter 5. Data types/5.6.autor_Calculator.js
+++ Chapter 5. Data types/5.6.autor_Calculator.js
@@ -5,6 +5,7 @@ Create a Calculator constructor function that creates "extensible" calculator ob
 The task consists of two parts.
 
 1. First, implement the calculate(str) method, which takes a string like "1 + 2" in the format NUM operator NUMBER (separated by spaces) and returns the result. The method must understand plus + and minus -.
+2. Then add the addMethod(name, func) method, which adds new operations to the calculator. It takes a name operator and a function with two arguments func(a,b) that describes it.
 
 */
 
