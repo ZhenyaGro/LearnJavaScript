f9e5365fc2a31e07a939a219bea4574ee01d56cd
ZhenyaGro <bon4ios@gmail.com>
2022-05-03 19:43:50 +0300

    Example how to ouse loadscript and callback

---
+++ Chapter 11. Async/test.js
@@ -0,0 +1,14 @@
+// How to download a script?
+
+'use strict';
+
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+
+  script.onload = () => callback(script);
+
+  document.head.append(script);
+}
+
+loadScript('./Chapter 10. Catch errors/1.1.autor_Finally.js'/*callback: , function() {}*/);
