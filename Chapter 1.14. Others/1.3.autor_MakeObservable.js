16141010449521bb1338e7300d1b768e21d321e7
ZhenyaGro <bon4ios@gmail.com>
2022-05-30 20:48:53 +0300

    Chapter 14.1

---
+++ Chapter 1.14. Others/1.3.autor_MakeObservable.js
@@ -0,0 +1,36 @@
+/*
+Create "makeObservable(target)" function
+that makes the object "observable" by returning a proxy.
+*/
+
+'use strict';
+
+let handlers = Symbol('handlers');
+
+function makeObservable(target) {
+  target[handlers] = [];
+
+  target.observe = function (handler) {
+    this[handlers].push(handler);
+  };
+
+  return new Proxy(target, {
+    set(target, property, value, receiver) {
+      let success = Reflect.set(...arguments);
+      if (success) {
+        target[handlers].forEach(handler => handler(property, value));
+      }
+      return success;
+    }
+  });
+}
+
+let user = {};
+
+user = makeObservable(user);
+
+user.observe((key, value) => {
+  alert(`SET ${key}=${value}`);
+});
+
+user.name = "John";
